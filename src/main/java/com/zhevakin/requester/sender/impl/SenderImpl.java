8e0f5684ffcd7ab1614ec30de9148c6192ba30c9
zhevakin <zhevakin@gloria-jeans.ru>
2022-09-13 10:56:15 +0300

    add SendRequester module

--- src/main/java/com/zhevakin/requester/sender/impl/SenderImpl.java
+++ src/main/java/com/zhevakin/requester/sender/impl/SenderImpl.java
@@ -2,74 +2,56 @@ package com.zhevakin.requester.sender.impl;
 
 
 import com.zhevakin.requester.model.Answer;
+import com.zhevakin.requester.sender.SendRequester;
 import com.zhevakin.requester.sender.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.Map;
 
 @Component
 public class SenderImpl implements Sender {
 
+    private final Map<HttpMethod, SendRequester> mapSenderRequester;
+
+    @Autowired
+    public SenderImpl(@Qualifier("senderMap") Map<HttpMethod, SendRequester> mapSenderRequester) {
+        this.mapSenderRequester = mapSenderRequester;
+    }
+
+    // TODO : переделать
     public Answer send(String url, Map<String, String> headers, Map<String, String> params,
                        HttpMethod httpMethod, String body) {
 
         Answer answer = new Answer();
-        final RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response;
+
         try {
-            HttpEntity<String> entity = new HttpEntity<>(body, getHeaders(headers));
-            switch (httpMethod) {
-                case GET: response = restTemplate.getForEntity(makeUrl(url,params), String.class); break;
-                case POST: response = restTemplate.postForEntity(makeUrl(url, params), entity, String.class); break;
-                default: response = restTemplate.exchange(makeUrl(url, params), httpMethod ,entity, String.class);
-            }
-        //    ResponseEntity<String> response = restTemplate.exchange(makeUrl(url, params), httpMethod, entity, String.class);
-            answer.setBody(response.getBody());
-            answer.setHttpStatus(response.getStatusCode());
-            answer.setHeaders(response.getHeaders());
-        } catch (Exception e) {
-            answer.setBody(e.getMessage());
-            answer.setHttpStatus(HttpStatus.FORBIDDEN);
-            answer.setHeaders(new HashMap<>());
+
+            answer = mapSenderRequester.get(httpMethod).execute(url, params, headers, body);
+        } catch (IOException ignored) {
+            return answer;
         }
-        return answer;
+
+       return answer;
 
 
     }
 
-    private HttpHeaders getHeaders(Map<String,String> headers) {
+//    private Header getHeaders(Map<String,String> headers) {
+//
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//
+//        for (Map.Entry<String, String> header : headers.entrySet()) {
+//            httpHeaders.put(header.getKey(), Collections.singletonList(header.getValue()));
+//        }
+//
+//        return httpHeaders;
+//
+//    }
 
-        HttpHeaders httpHeaders = new HttpHeaders();
 
-        for (Map.Entry<String, String> header : headers.entrySet()) {
-            httpHeaders.put(header.getKey(), Collections.singletonList(header.getValue()));
-        }
-
-        return httpHeaders;
-
-    }
-
-    private String makeUrl(String url, Map<String, String> params) {
-
-        StringBuilder stringBuilder = new StringBuilder(url);
-
-        stringBuilder.append('?');
-
-        for (Map.Entry<String, String> param : params.entrySet()) {
-            stringBuilder.append(param.getKey());
-            stringBuilder.append("=");
-            stringBuilder.append(param.getValue());
-            stringBuilder.append("&");
-        }
-
-        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-
-        return stringBuilder.toString();
-
-    }
 
 }
